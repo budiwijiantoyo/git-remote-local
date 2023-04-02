9cf58eb0ec3a905865c04b679ed5882a94292ce8
Budi Wijiantoyo <budi.wijiantoyo@gmail.com>
2023-04-02 17:02:57 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
     </head>
     <body>
         <h1>Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae voluptatibus nemo neque culpa at, repellendus autem nobis eius, dolores, officiis laborum tempora. Incidunt praesentium quaerat fuga perferendis possimus ut dolorem!</p>
     </body>
 </html>
