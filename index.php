26a690a6021cb01a7252d2727ab7742a7d89aed8
Budi Wijiantoyo <budi.wijiantoyo@gmail.com>
2023-04-02 17:09:57 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -6,5 +6,10 @@
     <body>
         <h1>Hello World!</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae voluptatibus nemo neque culpa at, repellendus autem nobis eius, dolores, officiis laborum tempora. Incidunt praesentium quaerat fuga perferendis possimus ut dolorem!</p>
+        <ul>
+            <li>Satu</li>
+            <li>Dua</li>
+            <li>Tiga</li>
+        </ul>
     </body>
 </html>
